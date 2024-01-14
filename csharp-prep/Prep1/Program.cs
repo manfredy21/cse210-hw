94e74b0e0ffacf1152db05f746ddc1124eecc69d
manfredy21 <manfredyantonioescobar@gmail.com>
2024-01-13 21:21:23 -0600

    activity

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -5,5 +5,10 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep1 World!");
+        Console.Write("What is you fist name?");
+        string first = Console.ReadLine();
+        Console.Write("What is you last name?");
+        string last = Console.ReadLine();
+        Console.WriteLine($"Your name is {last}, {first} {last}.");
     }
 }
--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,31 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        //Console.Write("what is the magic number?");
+        //string magicNumber = Console.ReadLine();
+        //int number = int.Parse(magicNumber);
+        Random randomNumber = new Random();
+        int number = randomNumber.Next(1,101);
+        
+
+        int guess = 0;
+
+        while (guess != number){
+            Console.Write("Guess the number");
+            guess = int.Parse(Console.ReadLine());
+
+            if(number > guess){
+                Console.WriteLine("The number is to lower than the magic number");
+            }
+            else if (number < guess)
+            {
+                Console.WriteLine("The number is to higher than the magic number");
+            }
+            else
+            {
+                Console.WriteLine("You guess the number!");
+            }
+        }
+
     }
 }
