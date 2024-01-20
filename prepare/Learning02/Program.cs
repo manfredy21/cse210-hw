ac7d2f30b6ded2671224fc22ef47c52207749597
manfredy21 <manfredyantonioescobar@gmail.com>
2024-01-20 17:27:44 -0600

    w2Activity1

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._jobTitle = "Proyectionist";
+        job1._company = "Cinepolis";
+        job1._startYear = 2018;
+        job1._endYear = 2019;
+
+        Job job2 = new Job();
+        job2._jobTitle = "Customer services representative";
+        job2._company = "IHG";
+        job2._startYear = 2020;
+        job2._endYear = 2024;
+
+        Resume myResume = new Resume();
+        myResume._name = "Manfredy Escobar";
+
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume.Display();
     }
 }
